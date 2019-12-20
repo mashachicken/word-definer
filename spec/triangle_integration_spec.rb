2be95c196edc3269ae6ff526ac926d5051b7fa5b
Masha Ivy <mashanechmo@gmail.com>
2019-12-20 12:20:09 -0800

    refactor code

--- spec/triangle_integration_spec.rb
+++ spec/triangle_integration_spec.rb
@@ -10,9 +10,20 @@ describe '#Word' do
   end
 
 
-    describe('.all') do
-      it("returns an empty array when there are no words") do
-        expect(Word.all).to(eq([]))
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('Word') do
+    describe('#save_word') do
+      it('saves a word to an array') do
+        Word.clear
+        word1 = Word.new("happy")
+        word1.save_word
+        expect(Word.all).to(eq([word1]))
       end
     end
-end 
+  end
+end
