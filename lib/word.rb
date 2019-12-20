1efbca796bf60e32d7bedada974ad5011f094892
Masha Ivy <mashanechmo@gmail.com>
2019-12-20 12:19:27 -0800

    refactor the code a little bit

--- lib/word.rb
+++ lib/word.rb
@@ -1,15 +1,23 @@
 class Word
   @@words = []
+  @@total_rows = 0
   attr_accessor(:definition)
 
-  def initialize(word, definition)
+  def initialize(word, id)
+    definition = []
+    @id = id || @@total_rows += 1
     @definition = definition
   end
-
+  def save
+    @@words[self.id] = Word.new(self.word, self.id)
+  end
+  def get_defined(definition)
+    @@words.push(definition)
+  end
   def self.all()
-    @@words.values()
+    @@words
   end
   def self.clear
-  @@words = {}
-end
+    @@words = []
+  end
 end
